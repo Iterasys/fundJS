6b961aee032ff58a6bde9356af8b1ce3c8971e55
Iterasys <40407006+Iterasys@users.noreply.github.com>
2022-10-14 21:51:37 -0300

    Exercicio de contagem de letras e palavras

--- src/funcoes.js
+++ src/funcoes.js
@@ -83,3 +83,82 @@ function contagemRegressiva(){
     document.getElementById("menu").innerHTML = resultado;
     
 }
+
+function desenharTapete(){
+    
+    let tapete = "";
+    const simbolo = "*";
+    const largura = 50;
+    const comprimento = 20;
+    
+
+    // snippet = trecho de código
+    for (let linha = 1; linha <= comprimento; linha++) {
+        for (let coluna = 1; coluna <= largura; coluna++) {
+            tapete += simbolo;   
+        }
+        tapete += "<br>"
+    }
+
+    // O que acontece
+    // coluna   tapete          coluna      tapete
+    //   1        ""               4        "###"
+    //   1        "#"              4        "####"
+    //   2        "#"              5        "####"
+    //   2        "##"             5        "#####"
+    //   3        "##"
+    //   3        "###"
+
+    document.getElementById("menu").innerHTML = tapete;
+
+}
+
+function contarLetrasEPalavras(){
+    const palavra = "gUARATINGUETA";
+    let contadorEspacos = 0;
+    let palavras = [];
+    let partePalavra = "";
+
+    resultado =  "A palavra tem " + palavra.length + " caracteres <br>";
+    resultado += "A palavra toda em maiusculas: " + palavra.toUpperCase() + "<br>";
+    resultado += "A palavra toda em minusculas: " + palavra.toLowerCase() + "<br>";
+    resultado += "A primeira letra: " + palavra.substring(0,1) + "<br>";
+    resultado += "A ultima letra: " + palavra.substring(palavra.length-1) + "<br>";
+    resultado += "A palavra com a primeira maiuscula e as restantes minusculas: " 
+               + palavra.substring(0,1).toUpperCase() + palavra.substring(1).toLowerCase() + "<br>";
+
+    const frase = "Ao longo do ceu vi espelhar o mar e me perdi no seu sorriso";
+
+    resultado += "A posicao do 1o espaco na frase: " + frase.indexOf(" ") + "<br>";
+    resultado += "A primeira palavra: " + frase.substring(0,frase.indexOf(" ")) + "<br>";
+    
+    for (let index = 0; index < frase.length; index++) {
+        const caracter = frase[index];
+            if (caracter == " ") {
+                palavras[contadorEspacos] = partePalavra;
+                partePalavra = "";
+                contadorEspacos++;               
+            }
+            else{
+                partePalavra += caracter;
+            }
+        
+    }
+
+    if (partePalavra.length != 0) {
+        palavras[contadorEspacos] = partePalavra;
+    }
+
+
+    resultado += "Existem " + contadorEspacos + " espacos na frase <br>";
+
+    for (let index = 0; index <= contadorEspacos; index++) {
+        resultado += palavras[index] + "<br>";
+        
+    }
+
+
+
+    document.getElementById("menu").innerHTML = resultado;
+
+}
